f406dc93d035521081dc368af473a6e1cc73bc88
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-07-14 01:05:01 +0200

    Using MongoDB as a backend

---
+++ 8/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const path = require("path");
+
+const adminRoutes = require("./routes/admin");
+const userRoutes = require("./routes/shop");
+
+const errorController = require("./controllers/404");
+const mongoConnect = require("./utils/database").mongoConnect;
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  // User.findByPk(1)
+  //   .then((user) => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch((err) => console.log(err));
+  next();
+});
+
+app.use("/admin", adminRoutes);
+app.use(userRoutes);
+
+app.use(errorController.getPageNotFound);
+
+mongoConnect(() => {
+  app.listen(3000);
+});
---
+++ 8/models/product.js
@@ -0,0 +1,34 @@
+const getDb = require("../utils/database").getDb;
+
+class Product {
+  constructor(title, price, imageUrl, description) {
+    this.title = title;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.description = description;
+  }
+
+  save() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .insertOne(this)
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        console.log(products);
+        return products;
+      })
+      .catch((err) => console.log(err));
+  }
+}
+
+module.exports = Product;
