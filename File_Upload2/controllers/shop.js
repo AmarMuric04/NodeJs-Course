7827da7382221b1f13a84ab8be5c7da887fc422d
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-10-15 18:15:13 +0200

    Exploring Pdf files

--- File_Upload2/controllers/shop.js
+++ File_Upload2/controllers/shop.js
@@ -1,5 +1,8 @@
 const Product = require("../models/product");
 const Order = require("../models/order");
+const fs = require("fs");
+const path = require("path");
+const PDFDocument = require("pdfkit");
 
 const { formatCurrency } = require("../util/money");
 
@@ -47,8 +50,15 @@ exports.getProduct = (req, res, next) => {
 exports.getIndex = (req, res, next) => {
   Product.find()
     .then((products) => {
+      const updatedPrices = products.map((e) => {
+        return {
+          ...e._doc,
+          price: formatCurrency(e.price),
+        };
+      });
+
       res.render("shop/index", {
-        prods: products,
+        prods: updatedPrices,
         pageTitle: "Shop",
         path: "/",
       });
@@ -154,4 +164,32 @@ exports.getOrders = (req, res, next) => {
       error.httpStatusCode = 500;
       return next(error);
     });
+};
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  Order.findById(orderId)
+    .then((order) => {
+      if (!order) {
+        return next(new Error("No order found."));
+      }
+      if (order.user.userId.toString() != req.session.user._id.toString()) {
+        return next(new Error("Unauthorized."));
+      }
+      const invoiceName = "invoice-" + orderId + ".pdf";
+      const invoicePath = path.join("data", "invoices", invoiceName);
+
+      const pdfDoc = new PDFDocument();
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+
+      pdfDoc.text("hello world!");
+      pdfDoc.end();
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader(
+        "Content-Disposition",
+        'inline; filename="' + invoiceName + '"'
+      );
+    })
+    .catch((err) => next(err));
 };
