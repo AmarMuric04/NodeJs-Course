4ff33f0a06501b5e4f6abf0ba1e6b50f20365cd3
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-10-21 22:58:07 +0200

    .

--- API/routes/feed.js
+++ API/routes/feed.js
@@ -4,6 +4,9 @@ const feedController = require("../controllers/feed");
 
 const router = express.Router();
 
+//GET /feed/posts
 router.get("/posts", feedController.getPosts);
+
+router.post("/post", feedController.postPost);
 
 module.exports = router;
